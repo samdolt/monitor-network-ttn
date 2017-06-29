f83413a505b403d73e739e013193290696488373
Samuel Dolt <samuel@dolt.ch>
2017-06-29 15:36:46 +0200

    update

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 sudo systemctl start redis
 sudo systemctl start influxdb
 sudo systemctl start chronograf
-TTN_APP_ID=bfhtest1 TTN_APP_ACCESS_KEY=ttn-account-v2.IN7kWMC4CxjsPW7NO6gcIYz8by7ai38pyIDtgXzYeBA ./monitor-network-ttn
+TTN_APP_ID=bfhtest1 TTN_APP_ACCESS_KEY=ttn-account-v2.REPLACE_ME_WITH_A_KEY ./monitor-network-ttn
